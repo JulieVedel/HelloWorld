9cffeb8a1c6e406b497504bd868cc6a5b25529f5
JulieVedel <juli76k0@edu.easj.dk>
2021-09-02 21:53:15 +0200

    Added variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+        String message = "Hello datamatikere!";
         System.out.println("Hello World!");
-        System.out.println("Hello datamatikere!");
+        System.out.println(message);
         System.out.println("Is this working?!");
 	    System.out.println("Any penguins?");
     }
